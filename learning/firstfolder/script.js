6491c9ed424b70fc5f710842ce985a7b4caa658e
Ermia Ghaffari <ermia@kth.se>
2024-03-16 20:15:31 +0100

    some more stuffs

--- learning/firstfolder/script.js
+++ learning/firstfolder/script.js
@@ -11,4 +11,39 @@ console.log(typeof ahmed); // Output: object
 console.log(ahmed.ermia);
 const ermia1 = 2;
 ermia1 = 3;
-console.log(ermia1);*/
+console.log(ermia1);
+console.log(12, 13);
+console.log(10 * 2);
+console.log(3 ** 4);
+const firstName = "Ermia";
+const lastName = "Ghaffari";
+const surNmae = firstName + " " + lastName;
+console.log(surNmae);
+// Assignemnt Operators
+let variable = 10;
+variable += 10;
+variable *= 2;
+console.log(variable);
+// Comparison operators
+*/
+// const firstName = "Ermia";
+// const lastName = " Ermia";
+// const birthDay = 2002;
+// const currentYear = 2024;
+// const ermia = "I'm" + " " + firstName + ", a " + (currentYear - birthDay) + "idiot";
+// console.log(ermia);
+// const ermiaNew = `I'm ${firstName}`;
+// console.log(ermiaNew);
+// console.log(`hello
+// my`);
+// console.log("Ermia \n\is not a good boy!")
+const age = 10;
+const isOldEnough = (age >= 18);
+if (isOldEnough) {
+    console.log("Your are allowed to get a driving licens");
+}
+else {
+    console.log(`you should come back in ${18 - age} years!😂`)
+}
+
+
